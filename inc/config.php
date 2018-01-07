a9d57feab776591a89f3d79e41bfd6fddfad5363
GeorgioLPB <georges.gregorio@gmail.com>
2018-01-07 18:42:27 +0100

    Use default cdn asset

--- inc/config.php
+++ inc/config.php
@@ -1,14 +1,14 @@
 <?php
 	define( 'PHP_MANUEL_URL', 'http://php.net/manual/en/opcache.configuration.php#ini.opcache.' ) ;
 
-	$cdn = 'georgio' ;
+	$cdn = 'default' ;
 
 	$cdn_depot = array(
 		'default' => array(
-			'bootstrap_css' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-			'bootstrap_js' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
-			'jquery_js' => 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
-			'chartjs_js' => 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js'
+			'bootstrap_css' => '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+			'bootstrap_js' => '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+			'jquery_js' => '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+			'chartjs_js' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js'
 		),
 		'georgio' => array(
 			'bootstrap_css' => '//cdn.georgio.fr/bootstrap/latest/css/bootstrap.min.css',
