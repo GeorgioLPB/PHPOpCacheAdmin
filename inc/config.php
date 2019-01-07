a4f8a74af320cbe77cdab40502ad41ef54a7aa5d
root <root@ns3009008.ip-151-80-44.eu>
2019-01-07 18:09:42 +0100

    one cdn ressources

--- inc/config.php
+++ inc/config.php
@@ -9,12 +9,6 @@
 			'bootstrap_js' => '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
 			'jquery_js' => '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
 			'chartjs_js' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js'
-		),
-		'georgio' => array(
-			'bootstrap_css' => '//cdn.georgio.fr/bootstrap/latest/css/bootstrap.min.css',
-			'bootstrap_js' => '//cdn.georgio.fr/bootstrap/latest/js/bootstrap.min.js',
-			'jquery_js' => '//cdn.georgio.fr/jquery/latest/jquery.min.js',
-			'chartjs_js' => '//cdn.georgio.fr/chartjs/latest/Chart.bundle.min.js'
 		)
 	) ;
 ?>
