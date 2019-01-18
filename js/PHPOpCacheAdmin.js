bf6ee78618adfe9d14137a07d06726675d69aecd
GeorgioLPB <georges.gregorio@gmail.com>
2019-01-18 10:49:34 +0100

    Add red color at 90%

--- js/PHPOpCacheAdmin.js
+++ js/PHPOpCacheAdmin.js
@@ -49,59 +49,58 @@ var myChart_opcachehitrate = new Chart(ctxopcachehitrate, {
 			borderWidth: 1
 		}]
 	},
-        options: { legend: { display: false } }
+	options: { legend: { display: false } }
 });
 
 var ctxstorage = document.getElementById("chart-storage").getContext('2d');
+var bgcolor_stg = [ 'rgba(92, 184, 92)', 'rgba(91, 192, 222)', 'rgba(240, 173, 78)' ] ;
+if( 90 < ( 100 * Number( $('#used_memory_value').text() ) / ( Number( $('#used_memory_value').text() ) + Number( $('#free_memory_value').text() ) + Number( $('#wasted_memory_value').text() ) ) ) )
+{ bgcolor_stg = [ 'rgba(255, 92, 92)', 'rgba(91, 192, 222)', 'rgba(240, 173, 78)' ] ; }
 var myChart_storage = new Chart(ctxstorage, {
 	type: 'pie',
 	data: {
 		labels: [ "Used Memory", "Free Memory", "Wasted Memory" ],
 		datasets: [{
 			data: [ $('#used_memory_value').text(), $('#free_memory_value').text(), $('#wasted_memory_value').text() ],
-			backgroundColor: [
-				'rgba(92, 184, 92)',
-				'rgba(91, 192, 222)',
-				'rgba(240, 173, 78)'
-			],
+			backgroundColor: bgcolor_stg,
 			borderWidth: 1
 		}]
 	},
-        options: { legend: { display: false } }
+	options: { legend: { display: false } }
 });
 
 var ctxisb = document.getElementById("chart-isb").getContext('2d');
+var bgcolor_isb = [ 'rgba(92, 184, 92)', 'rgba(91, 192, 222)' ] ;
+if( 90 < ( 100 * Number( $('#used_isb_value').text() ) / ( Number( $('#used_isb_value').text() ) + Number( $('#free_isb_value').text() ) ) ) )
+{ bgcolor_isb = [ 'rgba(255, 92, 92)', 'rgba(91, 192, 222)' ] ; }
 var myChart_isb = new Chart(ctxisb, {
 	type: 'pie',
 	data: {
 		labels: [ "Used Memory", "Free Memory" ],
 		datasets: [{
 			data: [ $('#used_isb_value').text(), $('#free_isb_value').text() ],
-			backgroundColor: [
-				'rgba(92, 184, 92)',
-				'rgba(91, 192, 222)'
-			],
+			backgroundColor: bgcolor_isb,
 			borderWidth: 1
 		}]
 	},
-        options: { legend: { display: false } }
+	options: { legend: { display: false } }
 });
 
 var ctxks = document.getElementById("chart-ks").getContext('2d');
+var bgcolor_ks = [ 'rgba(92, 184, 92)', 'rgba(91, 192, 222)' ] ;
+if( 90 < ( 100 * Number( $('#used_ks_value').text() ) / ( Number( $('#used_ks_value').text() ) + Number( $('#free_ks_value').text() ) ) ) )
+{ bgcolor_ks = [ 'rgba(255, 92, 92)', 'rgba(91, 192, 222)' ] ; }
 var myChart_ks = new Chart(ctxks, {
 	type: 'pie',
 	data: {
 		labels: [ "Cached Keys", "Free Keys" ],
 		datasets: [{
 			data: [ $('#used_ks_value').text(), $('#free_ks_value').text() ],
-			backgroundColor: [
-				'rgba(92, 184, 92)',
-				'rgba(91, 192, 222)'
-			],
+			backgroundColor: bgcolor_ks,
 			borderWidth: 1
 		}]
 	},
-        options: { legend: { display: false } }
+	options: { legend: { display: false } }
 });
 
 /* Actions */
