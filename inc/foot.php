e811529f61ee10c5da514854f158b4ed423b1c9d
GeorgioLPB <georges.gregorio@gmail.com>
2019-01-28 17:34:54 +0100

    Add GitHub link

--- inc/foot.php
+++ inc/foot.php
@@ -1,5 +1,11 @@
 	</div>
 
+	<footer class="footer">
+		<div class="container">
+			<span class="text-muted"><a href='https://github.com/GeorgioLPB/PHPOpCacheAdmin'><i class="fab fa-github"></i> GitHub</a></span>
+		</div>
+	</footer>
+
 	<script src="<?php echo $cdn_depot[$cdn]['jquery_js'] ; ?>"></script>
 	<script src="<?php echo $cdn_depot[$cdn]['bootstrap_js'] ; ?>"></script>
 	<script src="<?php echo $cdn_depot[$cdn]['datatables_js'] ; ?>"></script>
