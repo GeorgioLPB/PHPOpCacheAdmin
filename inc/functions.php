6bdbe96b54fd2647a886e4ad9d4b4af137717306
Debian <georges.gregorio@mail.com>
2024-02-02 19:21:33 +0100

    fix deprecated message for nbTime function

--- inc/functions.php
+++ inc/functions.php
@@ -1,47 +1,47 @@
 <?php
 function formatBytes( $bytes, $precision = 2 )
 {
-        //$units = array('B', 'KB', 'MB', 'GB', 'TB');
-        $units = array('b', 'Kb', 'Mb', 'Gb', 'Tb');
-        $bytes = max($bytes, 0);
-        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-        $pow = min($pow, count($units) - 1);
-        $bytes /= pow(1024, $pow);
-        return round($bytes, $precision) . ' ' . $units[$pow];
+	//$units = array('B', 'KB', 'MB', 'GB', 'TB');
+	$units = array('b', 'Kb', 'Mb', 'Gb', 'Tb');
+	$bytes = max($bytes, 0);
+	$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+	$pow = min($pow, count($units) - 1);
+	$bytes /= pow(1024, $pow);
+	return round($bytes, $precision) . ' ' . $units[$pow];
 }
 function formatMBytes( $bytes, $precision = 2 )
 {
-        //$units = array('MB', 'GB', 'TB');
-        $units = array('Mb', 'Gb', 'Tb');
-        $bytes = max($bytes, 0);
-        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-        $pow = min($pow, count($units) - 1);
-        $bytes /= pow(1024, $pow);
-        return round($bytes, $precision) . ' ' . $units[$pow];
+	//$units = array('MB', 'GB', 'TB');
+	$units = array('Mb', 'Gb', 'Tb');
+	$bytes = max($bytes, 0);
+	$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+	$pow = min($pow, count($units) - 1);
+	$bytes /= pow(1024, $pow);
+	return round($bytes, $precision) . ' ' . $units[$pow];
 }
 function formatInteger( $value, $unit = '' )
 {
-        switch( $unit )
-        {
-                case '':
-                        return number_format( $value, 0, ',', ' ' ) ;
-                        break;
-                case '%':
-                        return number_format( $value, 2, ',', ' ' )." $unit" ;
-                        break;
-                case 'files':
-                case 'keys':
-                case 'scripts':
-                case 'strings':
-                        return number_format( $value, 0, ',', ' ' )." ".$unit ;
-                        break;
-                case 'hexa':
-                        return "0x".strtoupper( dechex( $value ) ) ;
-                        break;
-                default :
-                        return "$value $unit" ;
-                        break;
-        }
+	switch( $unit )
+	{
+		case '':
+			return number_format( $value, 0, ',', ' ' ) ;
+			break;
+		case '%':
+			return number_format( $value, 2, ',', ' ' )." $unit" ;
+			break;
+		case 'files':
+		case 'keys':
+		case 'scripts':
+		case 'strings':
+			return number_format( $value, 0, ',', ' ' )." ".$unit ;
+			break;
+		case 'hexa':
+			return "0x".strtoupper( dechex( $value ) ) ;
+			break;
+		default :
+			return "$value $unit" ;
+			break;
+	}
 }
 function formatBoolean( $value )
 {
@@ -52,18 +52,16 @@ function formatBoolean( $value )
 function nbTime( $value )
 {
 	$bit = array(
-		'y' => $value / 31556926 % 12,
-		'w' => $value / 604800 % 52,
-		'd' => $value / 86400 % 7,
-		'h' => $value / 3600 % 24,
-		'm' => $value / 60 % 60,
-		's' => $value % 60
-        );
+		'y' => intval( $value / 31556926 ) % 12,
+		'w' => intval( $value / 604800 ) % 52,
+		'd' => intval( $value / 86400 ) % 7,
+		'h' => intval( $value / 3600 ) % 24,
+		'm' => intval( $value / 60 ) % 60,
+		's' => intval( $value ) % 60
+	) ;
 	foreach( $bit as $k => $v )
-        if( $v > 0 ) $ret[] = $v . $k ;
+	if( $v > 0 ) $ret[] = $v . $k ;
 
 	//var_dump( $bit ) ;
 	return join( ' ', $ret ) ;
 }
-
-?>
